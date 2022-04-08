d47fbd9bf582f047e5d86620bf2ef8207a53ca4c
Nathanael Wong <natgeo.wong@outlook.com>
2022-04-08 19:30:36 -0400

    Adding packages/functions to map and plot cubed-sphere results to lon-lat grids

---
+++ scripts/testplotCubedSphere.jl
@@ -0,0 +1,26 @@
+using DrWatson
+@quickactivate "AquaSlabIslands"
+
+using NCDatasets
+
+using PyCall, LaTeXStrings
+pplt = pyimport("proplot")
+
+include(srcdir("cubedsphere2lonlat.jl"))
+
+# ds  = NCDataset(datadir("domain.ocn.ne5np4_gx3v7.140810.nc"))
+ds  = NCDataset(datadir("domain.ocn.ne30_gx1v7.171003.nc"))
+lon = ds["xc"][:,1]
+lat = ds["yc"][:,1]
+afr = ds["frac"][:,1]
+close(ds)
+
+cs2ll = CubedSphere2LonLat(lon,lat,resolution_lon=0.2,resolution_lat=0.2)
+ndata = zeros(cs2ll.nlon,cs2ll.nlat)
+cubedsphere2lonlat!(ndata,afr,cs2ll)
+
+pplt.close(); f,a = pplt.subplots(axwidth=4,proj="moll")
+
+a[1].pcolormesh(cs2ll.lon,cs2ll.lat,ndata')
+
+f.savefig("test.png",transparent=false,dpi=300)
---
+++ src/cubedsphere2lonlat.jl
@@ -0,0 +1,62 @@
+using NearestNeighbors
+
+struct CubedSphere2LonLat{FT<:Real}
+
+    idxs :: Array{Int}
+    lon  :: Vector{FT}
+    lat  :: Vector{FT}
+    nlon :: Int
+    nlat :: Int
+
+end
+
+function CubedSphere2LonLat(
+    mesh_lon :: Vector{<:Real},
+    mesh_lat :: Vector{<:Real};
+    resolution_lon :: Real = 1.0,
+    resolution_lat :: Real = 1.0,
+    FT = Float64
+)
+
+    lon = collect( 0  : resolution_lon : 360); nlon = length(lon)
+    lat = collect(-90 : resolution_lat : 90);  nlat = length(lat)
+    ncell  = length(mesh_lon)
+    points = zeros(3,ncell)
+    idxs   = zeros(nlon,nlat)
+    
+    for icell = 1 : ncell
+        points[1,icell] = cosd(mesh_lon[icell]) * cosd(mesh_lat[icell])
+        points[2,icell] = sind(mesh_lon[icell]) * cosd(mesh_lat[icell])
+        points[3,icell] = sind(mesh_lat[icell])
+    end
+
+    kdtree = KDTree(points)
+
+    for ilat = 1 : nlat, ilon = 1 : nlon
+        x = cosd(lon[ilon]) * cosd(lat[ilat])
+        y = sind(lon[ilon]) * cosd(lat[ilat])
+        z = sind(lat[ilat])
+        idxs[ilon,ilat],_ = nn(kdtree,[x,y,z])
+    end
+
+    return CubedSphere2LonLat{FT}(idxs,lon,lat,nlon,nlat)
+
+end
+
+function cubedsphere2lonlat!(
+    oarray :: Array{<:Real,2},
+    iarray :: Vector{<:Real},
+    cs2ll  :: CubedSphere2LonLat{FT}
+) where FT <: Real
+
+    nlon = cs2ll.nlon
+    nlat = cs2ll.nlat
+    idxs = cs2ll.idxs
+
+    for ilat = 1 : nlat, ilon = 1 : nlon
+        oarray[ilon,ilat] = iarray[idxs[ilon,ilat]]
+    end
+
+    return
+
+end
