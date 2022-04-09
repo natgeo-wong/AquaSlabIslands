49e2314cebf4250082feb8ca5a66aa58113a4cce
Nathanael Wong <natgeo.wong@outlook.com>
2022-04-09 02:09:56 -0400

    Allowing us to save different FloatType formats

--- src/slaboceangen.jl
+++ src/slaboceangen.jl
@@ -3,7 +3,8 @@ using NCDatasets
 function slabocean_generation(
     fnc :: AbstractString;
     srcfile :: AbstractString,
-    control :: Bool = true
+    control :: Bool = true,
+    FT = Float32
 )
 
     cp(srcfile,fnc,force=true)
@@ -18,48 +19,48 @@ function slabocean_generation(
         "units"     => "days since 0001-01-01 00:00:00"
     ))
 
-    dsqdp = defVar(ds,"qdp",Float64,("ni","nj","time"),attrib=Dict(
+    dsqdp = defVar(ds,"qdp",FT,("ni","nj","time"),attrib=Dict(
         "long_name"   => "ocean heat flux convergence",
         "units"       => "W/m^2",
         "description" => "Qflux = 0 at every point"
     ))
 
-    dshblt = defVar(ds,"hblt",Float64,("ni","nj","time"),attrib=Dict(
+    dshblt = defVar(ds,"hblt",FT,("ni","nj","time"),attrib=Dict(
         "long_name"   => "ocean boundary layer depth (slab ocean thickness)",
         "units"       => "m",
     ))
 
-    dsS = defVar(ds,"S",Float64,("ni","nj","time"),attrib=Dict(
+    dsS = defVar(ds,"S",FT,("ni","nj","time"),attrib=Dict(
         "long_name"   => "salinity",
         "units"       => "ppt",
     ))
 
-    dsT = defVar(ds,"T",Float64,("ni","nj","time"),attrib=Dict(
+    dsT = defVar(ds,"T",FT,("ni","nj","time"),attrib=Dict(
         "long_name"   => "temperature",
         "units"       => "degC",
     ))
 
-    dsU = defVar(ds,"U",Float64,("ni","nj","time"),attrib=Dict(
+    dsU = defVar(ds,"U",FT,("ni","nj","time"),attrib=Dict(
         "long_name"   => "u ocean current",
         "units"       => "m/s",
     ))
 
-    dsV = defVar(ds,"V",Float64,("ni","nj","time"),attrib=Dict(
+    dsV = defVar(ds,"V",FT,("ni","nj","time"),attrib=Dict(
         "long_name"   => "v ocean current",
         "units"       => "m/s",
     ))
 
-    dsdhdx = defVar(ds,"dhdx",Float64,("ni","nj","time"),attrib=Dict(
+    dsdhdx = defVar(ds,"dhdx",FT,("ni","nj","time"),attrib=Dict(
         "long_name"   => "ocean surface slope: zonal",
         "units"       => "m/m",
     ))
 
-    dsdhdy = defVar(ds,"dhdy",Float64,("ni","nj","time"),attrib=Dict(
+    dsdhdy = defVar(ds,"dhdy",FT,("ni","nj","time"),attrib=Dict(
         "long_name"   => "ocean surface slope: meridional",
         "units"       => "m/m",
     ))
 
-    dstime.var[:] = [14.0,46.0,74.0,105.0,135.0,166.0,196.0,227.0,258.0,288.0,319.0,349.0]
+    dstime.var[:] = Float32.([14.0,46.0,74.0,105.0,135.0,166.0,196.0,227.0,258.0,288.0,319.0,349.0])
     dsqdp.var[:] .= 0
     dshblt.var[:] .= 0
     dsS.var[:] .= 0
